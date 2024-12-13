1eac7efd5b9e5d4f738293420b3411d478a38838
NeilRaiden <NeilRaidenLLC@gmail.com>
2024-12-12 20:31:35 -0500

    renamed spellchek to spellcheck

--- readlex/export-spell.sh
+++ readlex/export-spell.sh
@@ -10,20 +10,28 @@ awk -F'\t' '$5=="-" || $5=="UK"' KRL.tsv >KRL-UK.tsv
 echo "done"
 
 # ----- export Shavian column:
+echo -n "en.shav ..."
+awk -F'\t' '{print $3}' KRL.tsv  |sort |uniq >spellcheck/en.shav
+echo "done"
+
 echo -n "en_US.shav ..."
-awk -F'\t' '{print $3}' KRL-US.tsv  |sort |uniq >en_US.shav
+awk -F'\t' '{print $3}' KRL-US.tsv  |sort |uniq >spellcheck/en_US.shav
 echo "done"
 
 echo -n "en_UK.shav ..."
-awk -F'\t' '{print $3}' KRL-UK.tsv  |sort |uniq >en_UK.shav
+awk -F'\t' '{print $3}' KRL-UK.tsv  |sort |uniq >spellcheck/en_UK.shav
 echo "done"
 
 # ----- export IPA column:
+echo -n "en.ipa ..."
+awk -F'\t' '{print $2}' KRL.tsv  |sort |uniq >spellcheck/en.ipa
+echo "done"
+
 echo -n "en_US.ipa ..."
-awk -F'\t' '{print $2}' KRL-US.tsv  |sort |uniq >en_US.ipa
+awk -F'\t' '{print $2}' KRL-US.tsv  |sort |uniq >spellcheck/en_US.ipa
 echo "done"
 
 echo -n "en_UK.ipa ..."
-awk -F'\t' '{print $2}' KRL-UK.tsv  |sort |uniq >en_UK.ipa
+awk -F'\t' '{print $2}' KRL-UK.tsv  |sort |uniq >spellcheck/en_UK.ipa
 echo "done"
 
